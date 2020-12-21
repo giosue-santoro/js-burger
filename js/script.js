ad54f40e874179a92c1693a6a46ea720112af5d8
Giosuè Santoro <giosuesantoro@hotmail.it>
2020-12-21 17:44:22 +0100

    completato problema con sconto

--- js/script.js
+++ js/script.js
@@ -1,25 +1,52 @@
-//Assegnazione nome hamburger
-var hamburgerName = document.getElementById('hamburger-name');
-
-//Selezione di almeno due ingredienti
-
-//Selezione coupon - se presente -
-
-//Calcolo del prezzo - tenendo conto di sconto e ingredienti - al click del pulsante 'calculate'
 var cost = document.getElementsByClassName('ingredients-container')[0].getElementsByTagName('input');//variabile con checkbox
-
-var total = 0; //variabile totale
+var total = 0; //variabile costo totale
 var totalHtml = document.getElementById('total-price'); //variabile totale in HTML
+var discount = document.getElementById('discount'); //variabile sconto
+var discountPrice = 0;
 
 document.getElementById('price-button').addEventListener('click', function () { //Funzione per calcolare prezzo quando clicco sul pulsante
 
-  total = 0;
+  var empty = false; // variabile da utilizzare per i check di almeno due ingredienti e nome hamburger
 
-  for (var x = 0; x < cost.length; x++) {
-    if (cost[x].checked) {
-      total += parseInt(cost[x].value)
-    }
+  //Assegnazione nome hamburger obbligatoria
+  var hamburgerName = document.getElementById('hamburger-name');
+
+  if (hamburgerName.value === '') {
+    empty = true;
+    alert('Assegna un nome all\' hamburger');
   }
 
-  totalHtml.getElementsByTagName('span')[0].innerText = total + 50; // popolamento span in HTML con prezzo calcolato
+  //Calcolo del prezzo - tenendo conto di sconto e ingredienti - e check delle condizioni
+  total = 0;
+  ingredientCheck = 0;
+
+  if (empty === false) {
+    for (var x = 0; x < cost.length; x++) {
+      if (cost[x].checked) {
+        total += parseInt(cost[x].value);
+        ingredientCheck++;
+      }
+    }
+
+    if (ingredientCheck < 2) { //controllo che due ingredienti siano selezionati
+      empty = true;
+      alert('Non hai selezionato due ingredienti')
+    }
+
+    //Applicazione coupon - se presente -
+    var couponList = ['1234E','4321E','12345'];
+
+    if (empty === false) {
+      if (discount !== '') { //controllo che sia presente codice sconto
+        if (discount.value === couponList[0,1,2]) {
+          discountPrice = 20;
+          alert('Coupon accettato: sconto del 20% applicato');
+        }
+      }
+
+      total = (total + 50) - (total / 100 * discountPrice) + '$'; //Calcolo dello sconto
+
+      totalHtml.innerText = total; //Output in HTML
+    }
+  }
 })
